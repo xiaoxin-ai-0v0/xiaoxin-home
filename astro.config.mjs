6a53a5df08b38179c5349eccc9b0cb425c1bd8c1
Xie085 <88961069+Xie085@users.noreply.github.com>
2026-02-26 12:51:01 +0800

    配置 GitHub Pages 部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://xiaoxin-home.example.com',
+  site: 'https://xiaoxin-ai-0v0.github.io',
+  base: '/xiaoxin-home',
 });
