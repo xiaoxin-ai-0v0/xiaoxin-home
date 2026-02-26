4a09200f60cec52f4a8c78b217f748a262157774
Xie085 <88961069+Xie085@users.noreply.github.com>
2026-02-26 15:19:45 +0800

    配置 Cloudflare 适配器

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://xiaoxin-ai-0v0.github.io',
+  output: 'static',
+  adapter: cloudflare(),
 });
