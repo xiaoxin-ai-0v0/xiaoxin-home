725dee6101639d7fd624e1e747f8d0dd28fc8406
Xie085 <88961069+Xie085@users.noreply.github.com>
2026-02-26 18:16:49 +0800

    统一 base 配置为 /xiaoxin-home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-const isVercel = process.env.VERCEL === '1' || process.env.VERCEL === 'true';
-
 export default defineConfig({
   integrations: [tailwind()],
-  base: isVercel ? '' : '/xiaoxin-home',
+  base: '/xiaoxin-home',
   trailingSlash: 'always',
 });
