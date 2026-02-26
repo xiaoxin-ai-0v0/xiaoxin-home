2ee0a7eb976d3a8a806f1ba0bd295712b408ba1a
Xie085 <88961069+Xie085@users.noreply.github.com>
2026-02-26 16:53:01 +0800

    根据环境自动设置 base：Vercel 无 base，GitHub 有 base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const base = process.env.VERCEL ? '' : '/xiaoxin-home';
+
 export default defineConfig({
   integrations: [tailwind()],
+  base,
 });
